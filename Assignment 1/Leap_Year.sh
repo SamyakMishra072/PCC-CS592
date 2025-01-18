fc21c851f8a1118dac225962597ebe3b0c69511f
Samyak Mishra <115927401+SamyakMishra072@users.noreply.github.com>
2025-01-19 00:17:19 +0530

    Update Leap_Year.sh

--- Assignment 1/Leap_Year.sh
+++ Assignment 1/Leap_Year.sh
@@ -1,11 +1,14 @@
-read -p "Enter a year:" year
-if [ $((year % 400)) -eq 0 ]; then
-	echo "$year is a leap year:"
-elif [ $((year % 100)) -eq 0 ]; then
-	echo "$year is not a leap year"
-elif [ $((year % 4)) -eq 0 ]; then 
-	echo "$year is a leap year"
+read -p "Enter a year: " y
+if [ $y -le 0 ]; then
+        echo "INVALID INPUT"
+elif [ $((y%400)) -eq 0 ]; then
+        echo "LEAP YEAR"
+elif [ $((y%100)) -eq 0 ]; then
+        echo "NOT A LEAP YEAR"
+elif [ $((y%4)) -eq 0 ]; then
+        echo "LEAP YEAR"
 else
-	echo "$year is not a leap year:"
+        echo "NOT A LEAP YEAR"
 fi
 
+~
