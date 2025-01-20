9dde544e40fbf7dedab599ded5936523728c05c0
Samyak Mishra <115927401+SamyakMishra072@users.noreply.github.com>
2025-01-21 00:59:49 +0530

    Update Signal_Leap_Year.c

--- Assignment 7/Signal_Leap_Year.c
+++ Assignment 7/Signal_Leap_Year.c
@@ -2,50 +2,48 @@
 #include <signal.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/wait.h>
-int leap_year_check(int year) {
-    if (year % 4 == 0) {
-        if (year % 100 == 0) {
-            if (year % 400 == 0)
-                return 1; 
-            else
-                return 0;  
-        } else {
-            return 1; 
-        }
-    } else {
-        return 0; 
+#include <sys/types.h>
+
+int leap(int y) {
+    if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0)) {
+        return 1; // Leap year
     }
+    return 0; // Not a leap year
 }
+
 void handle_signal(int sig) {
-    int year;
-    printf("Child process received SIGUSR1. Enter a year to check if it's a leap year: ");
-    scanf("%d", &year);
-    if (leap_year_check(year)) {
-        printf("The year %d is a leap year.\n", year);
+    int y;
+    printf("\nChild Process received SIGUSR1. Enter a year to check: ");
+    scanf("%d", &y);
+
+    if (leap(y)) {
+        printf("%d is a leap year.\n", y);
     } else {
-        printf("The year %d is not a leap year.\n", year);
+        printf("%d is NOT a leap year.\n", y);
     }
 }
+
 int main() {
     pid_t pid;
     pid = fork();
+
     if (pid < 0) {
-        perror("Fork failed");
+        perror("FORK FAILED");
         exit(1);
     } else if (pid == 0) {
         signal(SIGUSR1, handle_signal);
-        printf("Child process started and waiting for SIGUSR1 from parent.\n");
+        printf("Child Process Started and waiting for SIGUSR1 from Parent.\n");
         while (1) {
-            pause(); 
+            pause();
         }
     } else {
-        printf("Parent process sending SIGUSR1 to child every 5 seconds.\n");
+        printf("Parent Process has Started and is Sending SIGUSR1 to the child every 5 seconds.\n");
         while (1) {
-            sleep(5);  
-            kill(pid, SIGUSR1);  
-            printf("Parent sent SIGUSR1 to child.\n");
+            sleep(5);
+            kill(pid, SIGUSR1);
+            printf("\nSIGUSR1 HAS BEEN SENT TO THE CHILD BY PARENT.\n");
         }
     }
+
     return 0;
 }
